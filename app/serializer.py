7b04d2762b429aa37312582a9f3e7fdfdc5f882b
veekthor04 <victoradenuga04@yahoo.com>
2022-03-08 07:56:54 +0100

    excluded engine__maker for anonymous user

--- app/serializer.py
+++ app/serializer.py
@@ -1,6 +1,8 @@
+from dataclasses import fields
 from django.contrib.auth import get_user_model
 from rest_framework import serializers, validators
-from .models import CarModel
+
+from app.models import CarModel, Engine
 
 
 class CarSerializer(serializers.ModelSerializer):
@@ -10,10 +12,18 @@ class CarSerializer(serializers.ModelSerializer):
         depth = 2
     #   lookup_field = "user_id"
     
-    
+
+class AnonymousEngineSerializer(serializers.ModelSerializer):
+    """EngineSerializer for an Anonymous user that excludes price"""
+    class Meta:
+        model = Engine
+        exclude = ['maker',]
+
+
 class AnonymousCarSerializer(serializers.ModelSerializer):
-    """CarSerializer for an Anonymous user that excludes price"""
+    """CarSerializer for an Anonymous user that excludes price and engine__maker"""
+    engine = AnonymousEngineSerializer()
+    
     class Meta:
         model = CarModel
-        exclude = ['price',]
-        depth = 2
+        fields = ['id', 'car_name', 'engine']
